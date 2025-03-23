015a1c2ef25bf572fa7995cc7bb205a118d62544
chris shortt <cshortt707@gmail.com>
2025-03-23 00:20:14 -0700

    almost done, but out of time

--- week008.py
+++ week008.py
@@ -1,3 +1,9 @@
+import psutil
+import os
+from sys import exit
+
+
+
 def vigenere_head(alphabet):
     return list(' ') + list(alphabet)
 
@@ -54,31 +60,63 @@ def decrypt_vigenere(key, cypher_text, alphabet):
             counter += 1
     return ''.join(plain_text)
 
+def enc_menu(key, alphabet, encrypted_list):
+    plaintext = input("Enter the text you'd like to encrypt: ")
+    encrypted_list.append(encrypt_vigenere(key, plaintext, alphabet))
+
+def dec_menu(key, alphabet, encrypted_list):
+    for cypher_text in encrypted_list:
+        print (cypher_text)
+
+    cypher_text = input("Enter the text you'd like to decrypt: ")
+
+    return decrypt_vigenere(key, cypher_text, alphabet)
+
+def dec_dump_menu(encrypted_list):
+    for cypher_text in encrypted_list:
+        print(cypher_text)
+def main():
+    key = 'rubiks'
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    message ='Resistance is Futile But Patterns Are Infinite'
+    encrypted_list = []
+    menu = [
+        ['1). Encrypt',enc_menu, [key, alphabet, encrypted_list]],
+        ['2). Decrypt', dec_menu, [key, alphabet, encrypted_list]],
+        ['3). Dump Decrypt', dec_dump_menu, [encrypted_list]],
+        ['4). Quit', exit, [0]]
+    ]
+    while True:
+        print ("_"*188)
+        for menu_item in menu:
+            print(menu_item[0])
+        try:
+            choice = int (input ("Make your choice: "))
+            if not (0 < choice  <= len(menu)):
+                print("././././././...YOU HAVE MADE A GRAVE ERROR ----Invalid choice")
+            else:
+                menu[choice -1][1](*menu[choice -1][2])
+        except ValueError as Ignored:
+                print("./././././.ERROR ALERT!!!Invalid choice, Enter an (INT) number betwixt 1 and 4 ")
+        encrypted_list.append(menu[1][1](*menu[1][2]))
+print(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2, "MB")
+    #for _ in range(2):
+        #menu[0][1](*menu[0][2])
+        #)
+
+    #menu[1][1](*menu[1][2])
+    #menu[2][1](*menu[2][2])
+    #menu[3][1](*menu[3][2])
 if __name__ == '__main__':
-    pass
+            main()
 
-
-
-
-key = 'rubiks'
-#message ='Resistance is Futile But Patterns Are Infinite'
-#message =iytqclrhdm sk wouqvw sou xklkysvc siy jvpaecum
-#🧊🔲🧊🔲🧊🔲🧊🔲🧊🔲🧊🔲🧊🔲🧊🔲🧊🔲🧊🔲🧊🔲
-# A proprietary mix of Borg inevitability and Rubik’s Cube's complexity of 43 quintillion possible Rubik’s Cube states. 🧊
-
-
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-message ='Resistance is Futile But Patterns Are Infinite'
-#vigenere_sq_print(vigenere_sq(alphabet))
-#print(vigenere_index('r','r',alphabet))
-ct = encrypt_vigenere(key, message, alphabet)
-print (ct,"     <---Encrypted Message")
-print (decrypt_vigenere(key, ct, alphabet), "     <-- Decrypted Message")
-
-
-
-
-
+    #vigenere_sq_print(vigenere_sq(alphabet))
+    #print(vigenere_index('r','r',alphabet))
+    #ct = encrypt_vigenere(key, message, alphabet)
+    #print (ct,"     <---Encrypted Message")
+    #print (decrypt_vigenere(key, ct, alphabet), "     <-- Decrypted Message")
 #print(encrypt_vigenere(key,message.lower(),alphabet))
 #print(len("Resistance is Futile But Patterns Are Infinite"))
 #print(len("iytqclrhdm sk wouqvw sou xklkysvc siy jvpaecum"))
+#🧊🔲🧊🔲🧊🔲🧊🔲🧊🔲🧊🔲🧊🔲🧊🔲🧊🔲🧊🔲🧊🔲
+
